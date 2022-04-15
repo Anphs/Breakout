9c2e5e258a2c1d43ef1a584b16abda7ef44e4e7b
Anthuony <brianpham907@gmail.com>
2022-04-14 22:39:44 -0500

    Initial commit

---
+++ desktop/src/me/anthuony/gdxtutorial/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package me.anthuony.gdxtutorial;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import me.anthuony.gdxtutorial.Tutorial;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("GdxTutorial");
+		new Lwjgl3Application(new Tutorial(), config);
+	}
+}
